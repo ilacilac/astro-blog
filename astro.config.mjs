aa2ad9ac53c7f28a0df93656b1a1ddbbe72009df
ilacilac <qkralswl307@gmail.com>
2025-04-29 15:51:37 +0900

    fix: 패키지 삭제

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import react from '@astrojs/react'
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-import netlify from '@astrojs/netlify'
 
 // https://astro.build/config
 export default defineConfig({
